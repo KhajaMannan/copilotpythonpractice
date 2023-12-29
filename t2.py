3821c31265f1f07d53d0bdd6d6ee844d9dce91bd
Khaja Mannanuddin <154660937+KhajaMannan@users.noreply.github.com>
2023-12-29 09:51:38 +0000

    pythoncodecovert

--- t2.py
+++ t2.py
@@ -1,7 +1,10 @@
 # Task: Create a function that reverses the words in a given sentence
-
+sentence ="Create a function that reverses the words in a given sentence"
 def reverse_words(sentence):
     words = sentence.split()
     # Use Copilot to complete the code to reverse the words in the sentence
     reversed_sentence = ' '.join(reversed(words))
     # TODO: Return the reversed sentence
+    print(reversed_sentence)
+
+reverse_words("Iam very positive minded with lot of tensions")
--- t3.py
+++ t3.py
@@ -2,7 +2,9 @@ import requests
 
 # Task: Make a GET request to 'https://api.example.com/data' and print the response JSON
 
-url = 'https://api.example.com/data'
+#url = 'https://google.com'
 # Use Copilot to complete the code to make the GET request and print the response JSON
 response = requests.get(url)
 # TODO: Print the response JSON
+print(response)
+print(response.json)
